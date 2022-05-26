dcbf25ff20ae4ec530e243bd1ad6453613d77f49
Rubens Lyra <rubensvlyra@gmail.com>
2022-05-26 08:03:59 -0300

    Convertendo string em data

---
+++ ConvertStringDate/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System.Globalization;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string date = "Jan 20,2019";
+    CultureInfo info = new CultureInfo("en-Us");
+    DateTime conDate = DateTime.Parse(date, info);
+    Console.WriteLine(conDate);
+    Console.ReadLine();
+  }
+}
